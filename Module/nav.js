bf3882b5263b1dc594eede2474f58e1b3632b477
MAWUNEYEFIA-KING <132532659+MAWUNEYEFIA-KING@users.noreply.github.com>
2024-10-07 09:45:44 +0000

    linking

--- Module/nav.js
+++
@@ -1,9 +0,0 @@
-// Load the navbar HTML
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('../../component')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar-container').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading navbar:', error));
-});
